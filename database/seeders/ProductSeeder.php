3fbe38c8f41cbf3fc6df44885b6222aa58706736
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-08 15:55:14 +0100

    feature : products table seeding

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Psy\Util\Str;
+use Ramsey\Uuid\Type\Decimal;
+use Ramsey\Uuid\Type\Integer;
 
 class ProductSeeder extends Seeder
 {
@@ -15,10 +18,11 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        DB:table('products')->insert([
-            'name'=>Str::random(15),
-            'description'=>Str::random(200),
-            'price'=>float::random(),
+        DB::table('products')->insert([
+            'name'=>'Pain aux céréales',
+            'description'=>'Pain croustillant aux graines de gourge et de lin',
+            'price'=> 1.95,
+            'stock'=>100,
     ]);
     }
 }
