1a80e73d784f9d944587f89ce007e99de1d31dfa
Maxime Kets <maximekets80@gmail.com>
2022-03-10 07:40:04 +0100

    Create Maxime's Home Branch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('categories')->insert([
+            'name' => 'Lorem',
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Lorem',
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Lorem',
+        ]);
     }
 }
---
+++ database/seeders/HomeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HomeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+            DB::table('categories')->insert([
+                'name' => 'Lorem',
+                'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles"
+            ]);
+        DB::table('categories')->insert([
+            'name' => 'Lorem',
+            'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles"
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Lorem',
+            'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles"
+        ]);
+    }
+}
