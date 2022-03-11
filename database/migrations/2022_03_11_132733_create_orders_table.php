486b9ca56ce85196904fe49b472543018aad12cf
vbruchon <vivian.brcuhon@le-campus-numerique.fr>
2022-03-11 16:13:05 +0100

    #9 Create Models

---
+++ database/migrations/2022_03_11_132733_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->timestamps();
+            $table->char('delivery_type', 45)->default('Click&Collect');
+        });
+
+        Schema::create('products_has_order', function (Blueprint $table) {
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
