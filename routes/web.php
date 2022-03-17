58ee71723f08bd32771c474a032242a43039f720
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-17 15:41:58 +0100

    feat: CartController - deletecart method and new paths

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,11 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products/category/{id}', [ProductController::class, 'categoryIndex'])->name('category');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('product');
 Route::post('/products', [CartController::class, 'store'])->name('addcart');
+
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
-Route::post('/cart', [CartController::class, 'remove'])->name('delete');
-Route::post('/updatecart', [CartController::class, 'updatecart'])->name('updatecart');
+Route::post('/cart', [CartController::class, 'update'])->name('updatecart');
+Route::post('/cart/remove', [CartController::class, 'remove'])->name('remove');
+Route::post('/cart/delete', [CartController::class, 'delete'])->name('deletecart');
 
 Route::get('/moncompte', [UserController::class, 'index'])->middleware(['auth'])->name('moncompte');
 
